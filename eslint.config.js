819266951dd2d7e156f7bee7f25743dd16281343
James Garbutt <43081j@users.noreply.github.com>
2025-04-10 11:52:59 +0100

    wip: first cut of cli

--- eslint.config.js
+++ eslint.config.js
@@ -12,7 +12,7 @@ export default defineConfig([
     },
     languageOptions: {
       globals: {
-        ...globals['shared-node-browser']
+        ...globals.node
       }
     },
     extends: [
--- src/cli.ts
+++ src/cli.ts
@@ -1,1 +1,32 @@
-console.log('foo');
+import {fdir} from 'fdir';
+import {cli, Command} from 'gunshi';
+import path from 'node:path';
+
+const command: Command = {
+  name: 'sourcemap-publisher',
+  description: 'Publishes sourcemaps externally',
+  options: {},
+  async run(ctx) {
+    const cwd = process.cwd();
+    const paths = (
+      ctx.positionals.length > 0 ? ctx.positionals : ['dist/']
+    ).map((p) => path.join(cwd, p));
+    const crawler = new fdir();
+    const files = await crawler
+      .withFullPaths()
+      .exclude((_dirName, dirPath) => {
+        return !paths.some((p) => dirPath.startsWith(p));
+      })
+      .filter((file) => {
+        return (
+          paths.some((p) => file.startsWith(p)) &&
+          (file.endsWith('.js') || file.endsWith('.ts'))
+        );
+      })
+      .crawl()
+      .withPromise();
+    console.log(files);
+  }
+};
+
+await cli(process.argv.slice(2), command);
