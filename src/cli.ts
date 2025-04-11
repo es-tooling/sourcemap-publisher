49a25bae49321ea31edffcabebc443e9e76891e6
James Garbutt <43081j@users.noreply.github.com>
2025-04-11 17:43:13 +0100

    feat: use sub-commands (#2)

--- src/cli.ts
+++ src/cli.ts
@@ -1,8 +1,18 @@
-import {cli} from 'gunshi';
+import {cli, define} from 'gunshi';
 import {publishCommand} from './commands/publish.js';
 
-await cli(process.argv.slice(2), publishCommand, {
+const subCommands = new Map([['publish', publishCommand]]);
+
+const mainCommand = define({
+  name: 'default',
+  run() {
+    console.log('No command specified. See --help for available commands.');
+  }
+});
+
+await cli(process.argv.slice(2), mainCommand, {
   name: 'sourcemap-publisher',
   version: '0.0.1',
-  description: 'Publishes sourcemaps externally'
+  description: 'Publishes sourcemaps externally',
+  subCommands
 });
