79d71e000a9a6efabaf10b7ba9ee23d8399ac055
James Garbutt <43081j@users.noreply.github.com>
2025-04-11 11:43:19 +0100

    wip: almost there

--- src/cli.ts
+++ src/cli.ts
@@ -1,10 +1,11 @@
 import {fdir} from 'fdir';
+import {x} from 'tinyexec';
 import {cli, define} from 'gunshi';
 import path from 'node:path';
-import * as prompt from '@clack/prompts';
-import {readFile, stat, writeFile} from 'node:fs/promises';
+import * as prompts from '@clack/prompts';
+import {readFile, stat, writeFile, mkdir, rm, cp} from 'node:fs/promises';
 
-async function getFiles(paths: string[]): Promise<string[]> {
+async function getFiles(cwd: string, paths: string[]): Promise<string[]> {
   const crawler = new fdir();
   const files = await crawler
     .withFullPaths()
@@ -18,78 +19,245 @@ async function getFiles(paths: string[]): Promise<string[]> {
           (!file.endsWith('.d.ts') && file.endsWith('.ts')))
       );
     })
-    .crawl()
+    .crawl(cwd)
     .withPromise();
   return files;
 }
 
+interface PackageJson {
+  files?: string[];
+  [key: string]: unknown;
+}
+
+const readPackageJson = async (p: string): Promise<PackageJson | null> => {
+  try {
+    const packageJson = await readFile(p, 'utf8');
+    return JSON.parse(packageJson);
+  } catch {
+    return null;
+  }
+};
+
+const filesToKeep = ['.npmrc', '.npmignore', 'package.json'];
+const getTempDir = async (p: string): Promise<string> => {
+  const tempDir = path.join(p, '.sourcemap-output');
+
+  await rm(tempDir, {force: true, recursive: true});
+  await mkdir(tempDir);
+
+  return tempDir;
+};
+const copyFilesToDir = async (
+  files: string[],
+  sourceDir: string,
+  targetDir: string
+): Promise<void> => {
+  for (const file of files) {
+    const sourcePath = path.join(sourceDir, file);
+    const targetPath = path.join(targetDir, file);
+    try {
+      await stat(sourcePath);
+      await cp(sourcePath, targetPath, {recursive: true});
+    } catch {
+      continue;
+    }
+  }
+};
+
+const updateSourceMapUrls = async (
+  cwd: string,
+  files: string[]
+): Promise<void> => {
+  // TODO (jg): maybe one day paralellise this with a concurrency limit
+  for (const file of files) {
+    let contents: string;
+
+    try {
+      contents = await readFile(file, 'utf8');
+    } catch {
+      prompts.log.warn(`Could not load file ${file}, skipping.`);
+      continue;
+    }
+
+    const lastLine = contents.slice(contents.lastIndexOf('\n') + 1);
+    const sourcemapPattern = /^\/\/# sourceMappingURL=(.+)/d;
+    const sourcemapMatch = lastLine.match(sourcemapPattern);
+
+    if (!sourcemapMatch || !sourcemapMatch.indices) {
+      continue;
+    }
+
+    const sourcemapURL = sourcemapMatch[1];
+
+    // Don't support absolute URLs, or URLs with a protocol
+    if (sourcemapURL.startsWith('/') || /^\w+:\/\//.test(sourcemapURL)) {
+      continue;
+    }
+
+    // Ignore inline maps
+    if (sourcemapURL.startsWith('data:')) {
+      continue;
+    }
+
+    const sourcemapPath = path.join(path.dirname(file), sourcemapURL);
+
+    try {
+      await stat(sourcemapPath);
+    } catch {
+      prompts.log.warn(
+        `Could not load sourcemap file ${sourcemapPath}, skipping.`
+      );
+      continue;
+    }
+
+    const sourcemapRelativePath = path.relative(cwd, sourcemapPath);
+    // TODO (43081j): get pkg-name from somewhere
+    const sourcemapNewPath = `https://example.com/pkg-name/${sourcemapRelativePath}`;
+
+    const newSourcemapLine =
+      lastLine.slice(0, sourcemapMatch.indices[1][0]) +
+      sourcemapNewPath +
+      lastLine.slice(sourcemapMatch.indices[1][1]);
+
+    await writeFile(
+      file,
+      contents.slice(0, contents.lastIndexOf('\n') + 1) + newSourcemapLine
+    );
+  }
+};
+
+const packageJsonKeysToStrip = ['exports'];
+
+const updatePackageJsonFiles = async (
+  cwd: string,
+  packageJsonPath: string,
+  packageJson: PackageJson,
+  paths: string[]
+): Promise<void> => {
+  const files: string[] = ['./stub.js'];
+  const newPackageJson: PackageJson = {
+    ...packageJson,
+    files,
+    main: './stub.js'
+  };
+
+  for (const path of paths) {
+    files.push(`${path}/**/*.map`);
+  }
+
+  for (const key of packageJsonKeysToStrip) {
+    newPackageJson[key] = undefined;
+  }
+
+  await writeFile(packageJsonPath, JSON.stringify(newPackageJson, null, 2));
+  await writeFile(path.join(cwd, './stub.js'), '');
+};
+
 const command = define({
   name: 'publish',
   description: 'Publishes sourcemaps externally',
-  options: {},
-  async run(ctx) {
-    const cwd = process.cwd();
-    const paths = (
-      ctx.positionals.length > 0 ? ctx.positionals : ['dist/']
-    ).map((p) => path.join(cwd, p));
-    const files = await getFiles(paths);
-    // TODO (jg): maybe one day paralellise this with a concurrency limit
-    for (const file of files) {
-      let contents: string;
-
-      try {
-        contents = await readFile(file, 'utf8');
-      } catch {
-        prompt.log.warn(`Could not load file ${file}, skipping.`);
-        continue;
-      }
-
-      const lastLine = contents.slice(contents.lastIndexOf('\n') + 1);
-      const sourcemapPattern = /^\/\/# sourceMappingURL=(.+)/d;
-      const sourcemapMatch = lastLine.match(sourcemapPattern);
-
-      if (!sourcemapMatch || !sourcemapMatch.indices) {
-        continue;
-      }
-
-      const sourcemapURL = sourcemapMatch[1];
-
-      // Don't support absolute URLs, or URLs with a protocol
-      if (sourcemapURL.startsWith('/') || /^\w+:\/\//.test(sourcemapURL)) {
-        continue;
-      }
-
-      // Ignore inline maps
-      if (sourcemapURL.startsWith('data:')) {
-        continue;
-      }
-
-      const sourcemapPath = path.join(path.dirname(file), sourcemapURL);
-
-      try {
-        await stat(sourcemapPath);
-      } catch {
-        prompt.log.warn(
-          `Could not load sourcemap file ${sourcemapPath}, skipping.`
-        );
-        continue;
-      }
-
-      const sourcemapRelativePath = path.relative(cwd, sourcemapPath);
-      // TODO (43081j): get pkg-name from somewhere
-      const sourcemapNewPath = `https://example.com/pkg-name/${sourcemapRelativePath}`;
-
-      const newSourcemapLine =
-        lastLine.slice(0, sourcemapMatch.indices[1][0]) +
-        sourcemapNewPath +
-        lastLine.slice(sourcemapMatch.indices[1][1]);
-
-      await writeFile(
-        file,
-        contents.slice(0, contents.lastIndexOf('\n') + 1) + newSourcemapLine
-      );
+  options: {
+    provenance: {
+      type: 'boolean',
+      description: 'Enable provenance when publishing to npm',
+      default: false
+    },
+    dryRun: {
+      type: 'boolean',
+      description: 'Dry run, do not publish',
+      default: false
     }
-    console.log(files);
+  },
+  async run(ctx) {
+    prompts.intro('Publishing sourcemaps...');
+
+    const cwd = process.cwd();
+    const paths = ctx.positionals.length > 0 ? ctx.positionals : ['dist/'];
+
+    const tempDir = await getTempDir(cwd);
+
+    try {
+      await copyFilesToDir([...filesToKeep, ...paths], cwd, tempDir);
+
+      const packageJsonPath = path.join(tempDir, 'package.json');
+      const packageJson = await readPackageJson(packageJsonPath);
+
+      if (packageJson === null) {
+        prompts.cancel(
+          'No package.json found. Please run this command in the project directory.'
+        );
+        return;
+      }
+
+      const resolvedPaths = paths.map((p) => path.join(tempDir, p));
+
+      const files = await getFiles(tempDir, resolvedPaths);
+
+      if (files.length === 0) {
+        prompts.cancel('No files were found to publish!');
+        return;
+      }
+
+      try {
+        await updateSourceMapUrls(tempDir, files);
+      } catch (err) {
+        prompts.log.error(`${err}`);
+        prompts.cancel('Failed to update sourcemap URLs');
+        return;
+      }
+
+      try {
+        await updatePackageJsonFiles(
+          tempDir,
+          packageJsonPath,
+          packageJson,
+          paths
+        );
+      } catch (err) {
+        prompts.log.error(`${err}`);
+        prompts.cancel('Failed to update package.json files');
+        return;
+      }
+
+      const npmArgs: string[] = ['publish', '--tag=sourcemaps'];
+
+      if (ctx.options.dryRun) {
+        npmArgs.push('--dry-run');
+      }
+
+      if (ctx.options.provenance) {
+        npmArgs.push('--provenance');
+      }
+
+      const log = prompts.taskLog({
+        title: 'Running `npm publish`',
+        limit: 10,
+        retainLog: true
+      });
+
+      try {
+        const publishProc = x('npm', npmArgs, {
+          nodeOptions: {
+            cwd: tempDir
+          }
+        });
+
+        for await (const line of publishProc) {
+          log.message(line);
+        }
+
+        log.success('`npm publish` succeeded', {showLog: true});
+      } catch (err) {
+        log.message(`${err}\n`, {raw: true});
+        log.error('Error running `npm publish`');
+        prompts.cancel('Failed to publish');
+        return;
+      }
+
+      prompts.outro('Published sourcemaps successfully!');
+    } finally {
+      await rm(tempDir, {force: true, recursive: true});
+    }
   }
 });
 
